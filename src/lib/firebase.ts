d4078b198f37758f752e619a56e7e2b2d049b18e
pkaushik-02002 <pdevulapally0202@gmail.com>
2025-07-22 16:03:05 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDoPQVUlcwEmsRaS-GiEg4qfB6SvEHhtyU",
-  authDomain: "subscription-tr.firebaseapp.com",
-  projectId: "subscription-tr",
-  storageBucket: "subscription-tr.firebasestorage.app",
-  messagingSenderId: "152263149627",
-  appId: "1:152263149627:web:f79b832592093aca6a741e",
-  measurementId: "G-K8KSTKXLBY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
